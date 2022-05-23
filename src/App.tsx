a7e3e52eff4d949beda37d2feba53e04de27ab41
Kaka <kakalition@gmail.com>
2022-05-23 12:13:10 +0700

    Create dynamic graph with generated dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(...registerables);
+
 export default function App() {
+  const [dataset, sDataset] = useState([150]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const newArr = [...dataset];
+      newArr.push(Math.floor(Math.random() * 50) + 100);
+      sDataset(newArr);
+    }, 1000);
+  }, [dataset]);
+
+  const data = {
+    labels: dataset,
+    datasets: [{
+      label: 'test label',
+      data: dataset,
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0,
+    }],
+  };
+
   return (
-    <div className="h-screen w-screen bg-white flex items-center justify-center" />
+    <div className="h-screen w-screen bg-white flex items-center justify-center p-16">
+      <Line data={data} />
+    </div>
   );
 }
